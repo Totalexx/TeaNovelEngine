6ef12f2b4270c7e14906a9715082a273fd961dbe
Shambonik <shambonik@gmail.com>
2020-06-11 13:41:55 +0300

    minor changes

--- android/src/com/crazyteaparty/teanovelengine/AndroidLauncher.java
+++ android/src/com/crazyteaparty/teanovelengine/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.crazyteaparty.teanovellaengine.Main;
+import com.crazyteaparty.teanovelengine.engine.Main;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
